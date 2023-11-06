57493c71d095a2be4eaee657e150164f3a1780ce
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-11-06 16:53:40 +0530

    Add files via upload

---
+++ day 1/leap year.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int f,s,i,siz,a=0;
+	cout << "enter 4 diget num1 : ";
+	cin >> f;
+	cout << "enter 4 diget num2 : ";
+	cin >> s;
+	siz=(s-f)/4+1;
+	int ar[siz];
+    if(999<f){
+        for(i=f; i<=s; i++){
+		    if(i%4==0)
+		    {
+			    ar[a++]=i;
+		    }
+		    
+		}
+		for(i=1; i<siz; i++){
+		    cout << ar[i] << " ";
+	    }
+	    
+    }
+    else{
+  	    cout << "enter 4 diget";
+    }
+	
+	
+	 
+}
+
